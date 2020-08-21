08db2933f50e5ab5badc09fedd76253a04342abe
Your Name <you@example.com>
2020-08-21 14:46:06 +0200

    Fix last

---
+++ docs/precache-manifest.f93d335fe0ebf6cc6d665aa0f6af6e45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e47651577544095badb6a9b9bfd4527",
+    "url": "/ghianda-parser/index.html"
+  },
+  {
+    "revision": "ce276d6ae1c8ea71db0a",
+    "url": "/ghianda-parser/static/js/2.edc8215a.chunk.js"
+  },
+  {
+    "revision": "72c798ca3ae37c30db01b9e0bb9f016e",
+    "url": "/ghianda-parser/static/js/2.edc8215a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46a3cbc4ded6cfdb41df",
+    "url": "/ghianda-parser/static/js/main.b47bd0a5.chunk.js"
+  },
+  {
+    "revision": "d471148ea373761f1984",
+    "url": "/ghianda-parser/static/js/runtime-main.c602b906.js"
+  }
+]);
